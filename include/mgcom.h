50cf15f96b33f27fc6b9ff852acbf1e06fe84fce
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-07-14 19:37:12 +0900

    Update API

--- include/mgcom.h
+++ include/mgcom.h
@@ -40,29 +40,57 @@ bool mgcom_finalize(void);
 /**
  * Register a region located on the current process.
  */
-mgcom_local_addr_t mgcom_register_local_region(void* ptr, mgcom_size_t size, mgcom_local_region_id_t* region_id_result);
+bool mgcom_register_local_region(
+    void*                    local_ptr,
+    mgcom_size_t             size_in_bytes,
+    mgcom_local_region_id_t* region_id_result,
+    mgcom_local_addr_t*      region_addr_result
+);
 
 /**
- * Register a region located on a remote process.
+ * Prepare a region located on a remote process.
  */
-mgcom_remote_addr_t mgcom_register_remote_region(mgcom_process_id_t proc_id, void* ptr, mgcom_size_t size, mgcom_remote_region_id_t* region_id_result);
+bool mgcom_use_remote_region(
+    mgcom_process_id_t        proc_id,
+    mgcom_local_region_id_t   local_region_id,
+    void*                     remote_ptr,
+    mgcom_size_t              size_in_bytes,
+    mgcom_remote_region_id_t* region_id_result,
+    mgcom_remote_addr_t*      region_addr_result
+);
+
+/**
+ * De-register the region located on the current process.
+ */
+void mgcom_deregister_local_region(
+    void*                    local_ptr,
+    mgcom_size_t             size_in_size,
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr
+);
 
 /**
  * Non-blocking contiguous "put".
  */
 bool mgcom_put_async(
-    mgcom_local_region_id_t local_region_id, mgcom_local_addr_t local_addr,
-    mgcom_remote_region_id_t remote_region_id, mgcom_remote_addr_t remote_addr,
-    mgcom_process_id_t dest_proc, mgcom_handle_t* handle
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr,
+    mgcom_remote_region_id_t remote_region_id,
+    mgcom_remote_addr_t      remote_addr,
+    mgcom_process_id_t       dest_proc,
+    mgcom_handle_t*          handle
 );
 
 /**
  * Non-blocking contiguous "get".
  */
 bool mgcom_get_async(
-    mgcom_local_region_id_t local_region_id, mgcom_local_addr_t local_addr,
-    mgcom_remote_region_id_t remote_region_id, mgcom_remote_addr_t remote_addr,
-    mgcom_process_id_t src_proc, mgcom_handle_t* handle
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr,
+    mgcom_remote_region_id_t remote_region_id,
+    mgcom_remote_addr_t      remote_addr,
+    mgcom_process_id_t       dest_proc,
+    mgcom_handle_t*          handle
 );
 
 typedef struct {
@@ -78,42 +106,67 @@ mgcom_vector_access_t;
 /**
  * Non-blocking vector "put".
  */
-bool mgcom_put_vector_async(mgcom_vector_access_t* accesses, mgcom_size_t num_accesses, mgcom_process_id_t dest_proc, mgcom_handle_t* handle);
+bool mgcom_put_vector_async(
+    mgcom_vector_access_t* accesses,
+    mgcom_size_t           num_accesses,
+    mgcom_process_id_t     dest_proc,
+    mgcom_handle_t*        handle
+);
 
 /**
  * Non-blocking vector "get".
  */
-bool mgcom_get_vector_async(mgcom_vector_access_t* accesses, mgcom_size_t num_accesses, mgcom_process_id_t src_proc, mgcom_handle_t* handle);
+bool mgcom_get_vector_async(
+    mgcom_vector_access_t* accesses,
+    mgcom_size_t           num_accesses,
+    mgcom_process_id_t     src_proc,
+    mgcom_handle_t*        handle
+);
 
 /**
  * Non-blockng strided "put".
  */
 bool mgcom_put_strided_async(
-    mgcom_local_region_id_t local_region_id, mgcom_local_addr_t local_addr, mgcom_size_t* local_stride,
-    mgcom_remote_region_id_t remote_region_id, mgcom_remote_addr_t remote_addr, mgcom_size_t* remote_stride,
-    mgcom_size_t* count, mgcom_size_t stride_level,
-    mgcom_process_id_t dest_proc, mgcom_handle_t* handle
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr,
+    mgcom_size_t*            local_stride,
+    mgcom_remote_region_id_t remote_region_id,
+    mgcom_remote_addr_t      remote_addr,
+    mgcom_size_t*            remote_stride,
+    mgcom_size_t*            count,
+    mgcom_size_t             stride_level,
+    mgcom_process_id_t       dest_proc,
+    mgcom_handle_t*          handle
 );
 
 /**
  * Non-blockng strided "get".
  */
 bool mgcom_get_strided_async(
-    mgcom_local_region_id_t local_region_id, mgcom_local_addr_t local_addr, mgcom_size_t* local_stride,
-    mgcom_remote_region_id_t remote_region_id, mgcom_remote_addr_t remote_addr, mgcom_size_t* remote_stride,
-    mgcom_size_t* count, mgcom_size_t stride_level,
-    mgcom_process_id_t src_proc, mgcom_handle_t* handle
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr,
+    mgcom_size_t*            local_stride,
+    mgcom_remote_region_id_t remote_region_id,
+    mgcom_remote_addr_t      remote_addr,
+    mgcom_size_t*            remote_stride,
+    mgcom_size_t*            count,
+    mgcom_size_t             stride_level,
+    mgcom_process_id_t       src_proc,
+    mgcom_handle_t*          handle
 );
 
 /**
- * Remote atomic operation.
+ * Non-blocking remote atomic operation.
  */
-bool mgcom_rmw(
-    mgcom_operation_t op,
-    void* local_expected,
-    mgcom_local_region_id_t local_region_id, mgcom_local_addr_t local_addr,
-    mgcom_remote_region_id_t remote_region_id, mgcom_remote_addr_t remote_addr,
-    mgcom_process_id_t dest_proc, mgcom_handle_t* handle
+bool mgcom_rmw_async(
+    mgcom_operation_t        op,
+    void*                    local_expected,
+    mgcom_local_region_id_t  local_region_id,
+    mgcom_local_addr_t       local_addr,
+    mgcom_remote_region_id_t remote_region_id,
+    mgcom_remote_addr_t      remote_addr,
+    mgcom_process_id_t       dest_proc,
+    mgcom_handle_t*          handle
 );
 
 /**
