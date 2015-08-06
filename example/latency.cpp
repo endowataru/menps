763b94c3793d5dec4944868e28e958790649b059
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-08-06 10:42:31 +0900

    Add missing example

---
+++ example/latency.cpp
@@ -0,0 +1,57 @@
+
+#include <mgcom.hpp>
+#include <mgbase/stdint.hpp>
+#include <mpi.h>
+#include <mgbase/profiling/stopwatch.hpp>
+
+int main(int argc, char* argv[])
+{
+    using namespace mgcom;
+    
+    initialize(&argc, &argv);
+    
+    int x = 0;
+    local_region_t local_region = register_region(&x, sizeof(x));
+    
+    region_key_t remote_key;
+    
+    process_id_t current = current_process_id();
+    process_id_t other = 1 - current_process_id();
+    MPI_Sendrecv(&local_region.key, sizeof(region_key_t), MPI_BYTE,
+                 static_cast<int>(other), 0,
+                 &remote_key, sizeof(region_key_t), MPI_BYTE,
+                 static_cast<int>(other), 0,
+                 MPI_COMM_WORLD, MGBASE_NULLPTR);
+    
+    local_address_t local_addr = { local_region, 0 };
+    remote_address_t remote_addr = { use_remote_region(other, remote_key, sizeof(x)), 0 };
+    
+    mgbase::stopwatch sw;
+    
+    if (current == 0) {
+        x = 123;
+        
+        sw.start();
+        bool finished = false;
+        while (!try_write_async(local_addr, remote_addr, sizeof(x), other, make_notifier_assign(&finished, true))) {
+            std::cout << "write failed" << std::endl;
+            poll();
+        }
+        
+        while (!finished) {
+            std::cout << "poll" << std::endl;
+            poll();
+        }
+    }
+    
+    barrier();
+    
+    std::cout << x << std::endl;
+    std::cout << sw.elapsed() << std::endl;
+    
+    
+    finalize();
+    
+    return 0;
+}
+
