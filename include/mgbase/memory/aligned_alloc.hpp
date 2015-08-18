478d27ffc5ed92d251b179500b3c3cd2ba3c0860
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-08-19 01:05:39 +0900

    Add aligned_alloc

---
+++ include/mgbase/memory/aligned_alloc.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <mgbase/lang.hpp>
+#include <cstdlib>
+
+namespace mgbase {
+
+
+namespace {
+
+inline void* aligned_alloc(std::size_t alignment, std::size_t size) {
+    #ifdef MGBASE_CPP11_SUPPORTED
+        return std::aligned_alloc();
+    #else
+        // POSIX
+        void* result;
+        posix_memalign(&result, alignment, size);
+        return result;
+    #endif
+}
+
+}
+
+}
+
