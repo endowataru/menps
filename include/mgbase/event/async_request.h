75e162cdf80326762966ee34bd071dad2241c451
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-08-26 02:26:51 +0900

    Change type of async_request::state

--- include/mgbase/event/async_request.h
+++ include/mgbase/event/async_request.h
@@ -5,11 +5,11 @@
 
 MGBASE_EXTERN_C_BEGIN
 
-#define MGBASE_STATE_FINISHED        static_cast<mgbase::size_t>(-1)
+#define MGBASE_STATE_FINISHED        static_cast<uint32_t>(-1)
 
 struct mgbase_async_request {
     void (*func_test)(void*);
-    size_t state;
+    uint32_t state;
 };
 
 MGBASE_EXTERN_C_END
