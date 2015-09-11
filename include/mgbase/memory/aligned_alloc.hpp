00dbac71e679b52c02a409c9e7929ae7bdddc302
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-09-11 16:54:20 +0900

    Fix aligned_alloc

--- include/mgbase/memory/aligned_alloc.hpp
+++ include/mgbase/memory/aligned_alloc.hpp
@@ -2,21 +2,40 @@
 #pragma once
 
 #include <mgbase/lang.hpp>
+#include <mgbase/assert.hpp>
+#include <mgbase/arithmetic.hpp>
 #include <cstdlib>
+#include <iostream>
 
 namespace mgbase {
 
+struct alloc_error { };
+
 namespace {
 
+
 inline void* aligned_alloc(std::size_t alignment, std::size_t size) {
+    MGBASE_ASSERT(alignment > 0);
+    MGBASE_ASSERT(size > 0);
+    MGBASE_ASSERT(size % alignment == 0);
+    
+    void* result;
     #ifdef MGBASE_CPP11_SUPPORTED
-        return std::aligned_alloc(alignment, size);
+        result = std::aligned_alloc(alignment, size);
     #else
+        if (alignment < sizeof(void*)) {
+            // Fix the alignment.
+            alignment = sizeof(void*);
+            size = roundup_divide(size, sizeof(void*)) * sizeof(void*);
+        }
+        
         // POSIX
-        void* result;
-        posix_memalign(&result, alignment, size);
-        return result;
+        int ret = posix_memalign(&result, alignment, size);
+        if (ret != 0)
+            throw alloc_error();
     #endif
+    MGBASE_ASSERT(result != MGBASE_NULLPTR);
+    return result;
 }
 
 }
