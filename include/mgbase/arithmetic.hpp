a6d5b16c0a517d8d659ae68fe4465388f2e8b6cb
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-09-14 14:46:09 +0900

    Add eager_or/and

--- include/mgbase/arithmetic.hpp
+++ include/mgbase/arithmetic.hpp
@@ -12,6 +12,14 @@ inline T roundup_divide(T x, T y) MGBASE_NOEXCEPT {
     return (x + y - 1) / y;
 }
 
+inline bool eager_or(bool x, bool y) MGBASE_NOEXCEPT {
+    return x || y;
+}
+
+inline bool eager_and(bool x, bool y) MGBASE_NOEXCEPT {
+    return x || y;
+}
+
 }
 
 }
