2dde1562c712371dfddefe98e15a71bd187ce450
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-10-11 15:12:51 +0900

    Add meta functions to type_traits (standardized in C++11)

--- include/mgbase/type_traits.hpp
+++ include/mgbase/type_traits.hpp
@@ -22,6 +22,114 @@ struct identity {
     typedef T   type;
 };
 
+template <typename T>
+T declval() MGBASE_NOEXCEPT; // not defined
+
+template <typename T, T v>
+struct integral_constant
+{
+    static const T value = v;
+    
+    typedef T                   value_type;
+    typedef integral_constant   type;
+    
+    operator value_type() const MGBASE_NOEXCEPT { return value; }
+};
+
+typedef integral_constant<bool, true>   true_type;
+typedef integral_constant<bool, false>  false_type;
+
+template <typename T, typename U>
+struct is_same : false_type {};
+
+template <typename T>
+struct is_same<T, T> : true_type {};
+ 
+template <typename T> struct remove_const          { typedef T type; };
+template <typename T> struct remove_const<const T> { typedef T type; };
+ 
+template <typename T> struct remove_volatile             { typedef T type; };
+template <typename T> struct remove_volatile<volatile T> { typedef T type; };
+
+template <typename T>
+struct remove_cv {
+    typedef typename remove_volatile<typename remove_const<T>::type>::type type;
+};
+
+template <typename T >
+struct is_void : is_same<void, typename remove_cv<T>::type> { };
+
+template <typename> struct is_integral             : false_type { };
+template <> struct is_integral<bool>               : true_type { };
+template <> struct is_integral<char>               : true_type { };
+template <> struct is_integral<signed char>        : true_type { };
+template <> struct is_integral<wchar_t>            : true_type { };
+template <> struct is_integral<short>              : true_type { };
+template <> struct is_integral<int>                : true_type { };
+template <> struct is_integral<long>               : true_type { };
+template <> struct is_integral<long long>          : true_type { };
+template <> struct is_integral<unsigned char>      : true_type { };
+template <> struct is_integral<unsigned short>     : true_type { };
+template <> struct is_integral<unsigned int>       : true_type { };
+template <> struct is_integral<unsigned long>      : true_type { };
+template <> struct is_integral<unsigned long long> : true_type { };
+
+template <typename> struct is_floating_point      : false_type { };
+template <> struct is_floating_point<float>       : true_type { };
+template <> struct is_floating_point<double>      : true_type { };
+template <> struct is_floating_point<long double> : true_type { };
+
+template <typename T>
+struct is_arithmetic
+    : integral_constant<bool,
+        is_integral<T>::value || is_floating_point<T>::value
+    > { };
+
+template <typename T>
+struct is_fundamental
+    : integral_constant<bool,
+        is_arithmetic<T>::value || is_void<T>::value
+    > { };
+
+template <typename T>
+struct is_compound
+    : integral_constant<bool,
+        !is_fundamental<T>::value
+    > { };
+
+// is_convertible
+// From Boost.TypeTraits
+
+namespace detail {
+
+struct yes_type { char x;    };
+struct no_type  { char x[2]; };
+
+struct any_conversion
+{
+    template <typename T> any_conversion(const volatile T&);
+    template <typename T> any_conversion(const T&);
+    template <typename T> any_conversion(volatile T&);
+    template <typename T> any_conversion(T&);
+};
+
+template <typename From, typename To>
+struct is_convertible_impl
+{
+    static no_type  check(any_conversion, ...);
+    static yes_type check(To, int);
+    
+    static const bool value = sizeof(check(declval<From>())) == sizeof(yes_type);
+};
+
+}
+
+template <typename From, typename To>
+struct is_convertible
+{
+    static const bool value = detail::is_convertible_impl<From, To>::value;
+};
+
 }
 
 #else
@@ -31,6 +139,8 @@ struct identity {
 namespace mgbase {
 
 using std::enable_if;
+
+using std::is_fundamental;
 
 }
 
