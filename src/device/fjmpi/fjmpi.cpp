2029d31c583206ddcb7d26ba01cacbeef26cf5e5
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-10-17 07:11:35 +0900

    Add missing files

---
+++ src/device/fjmpi/fjmpi.cpp
@@ -0,0 +1,45 @@
+
+#include "common/mpi_base.hpp"
+#include "rma/rma.hpp"
+#include "am/am.hpp"
+
+#include <mgbase/logging/logger.hpp>
+
+namespace mgcom {
+
+namespace /*unnamed*/ {
+
+std::string get_state() {
+    static index_t number = 0;
+    fmt::MemoryWriter w;
+    w.write("proc:{}\tlog_id:{}\t", current_process_id(), number++);
+    return w.str();
+}
+
+}
+
+void initialize(int* argc, char*** argv)
+{
+    mpi_base::initialize(argc, argv);
+    rma::initialize();
+    am::initialize();
+    
+    mgbase::logger::set_state_callback(get_state);
+    
+    MGBASE_LOG_DEBUG("msg:Initialized.");
+}
+
+void finalize()
+{
+    barrier(); 
+    
+    am::finalize();
+    rma::finalize();
+    
+    mpi_base::finalize();
+    
+    MGBASE_LOG_DEBUG("msg:Finalized.");
+}
+
+}
+
