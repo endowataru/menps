b04b5f347d65d599f7e5fcb1753f6fae72b56ea0
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-11-10 21:46:34 +0900

    Add pointer_facade and runtime_sized

---
+++ include/mgbase/pointer_facade.hpp
@@ -0,0 +1,205 @@
+
+#pragma once
+
+#include <mgbase/lang.hpp>
+#include <mgbase/type_traits.hpp>
+
+#include <cstring>
+
+namespace mgbase {
+
+/**
+ * If the type From is convertible to the type T, "value" is true.
+ */
+template <typename From, typename To>
+struct pointer_convertible
+    : mgbase::is_convertible<From*, To*> { };
+
+// declarations
+
+template <template <typename> class Derived, typename T>
+class pointer_facade;
+
+template <typename To, template <typename> class Derived, typename From>
+inline
+typename mgbase::enable_if<
+    pointer_convertible<From, To>::value
+,   Derived<To>
+>::type
+implicit_pointer_cast(const pointer_facade<Derived, From>&);
+
+template <typename To, template <typename> class Derived, typename From>
+inline Derived<To>
+reinterpret_pointer_cast(const pointer_facade<Derived, From>& ptr);
+
+namespace detail
+{
+    template <template <typename> class Derived, typename T, bool HasMembers>
+    class pointer_facade_member;
+    
+    template <template <typename> class Derived, typename T>
+    class pointer_facade_operators;
+}
+
+/// Transfer class for private members of pointer_facade
+
+class pointer_core_access
+{
+private:
+    template <template <typename> class Derived, typename T>
+    static const Derived<T>& derived(const pointer_facade<Derived, T>& ptr) {
+        return static_cast<const Derived<T>&>(ptr);
+    }
+    
+    template <typename U, template <typename> class Derived, typename T>
+    static Derived<U> cast_to(const pointer_facade<Derived, T>& ptr) {
+        return derived(ptr).template cast_to<U>();
+    }
+    
+    template <typename Derived>
+    static void advance(Derived& ptr, std::ptrdiff_t diff) {
+        ptr.advance(diff);
+    }
+    
+    template <template <typename> class Derived, typename T, bool HasMembers>
+    friend class detail::pointer_facade_member;
+    
+    template <template <typename> class Derived, typename T>
+    friend class detail::pointer_facade_operators;
+    
+    template <template <typename> class Derived, typename T>
+    friend class pointer_facade;
+    
+    template <typename To, template <typename> class Derived, typename From>
+    friend typename mgbase::enable_if<
+        pointer_convertible<From, To>::value
+    ,   Derived<To>
+    >::type
+    implicit_pointer_cast(const pointer_facade<Derived, From>& ptr);
+    
+    template <typename To, template <typename> class Derived, typename From>
+    friend Derived<To>
+    reinterpret_pointer_cast(const pointer_facade<Derived, From>& ptr);
+};
+
+template <typename To, template <typename> class Derived, typename From>
+inline
+typename mgbase::enable_if<
+    pointer_convertible<From, To>::value
+,   Derived<To>
+>::type
+implicit_pointer_cast(const pointer_facade<Derived, From>& ptr) {
+    return pointer_core_access::cast_to<To>(ptr);
+}
+
+template <typename To, template <typename> class Derived, typename From>
+inline Derived<To>
+reinterpret_pointer_cast(const pointer_facade<Derived, From>& ptr) {
+    return pointer_core_access::cast_to<To>(ptr);
+}
+
+namespace detail {
+
+template <template <typename> class Derived, typename T,
+    bool HasMembers =
+        mgbase::is_compound<typename mgbase::remove_cv<T>::type>::value
+        && !mgbase::is_array<typename mgbase::remove_cv<T>::type>::value
+>
+class pointer_facade_member { };
+
+template <template <typename> class Derived, typename T>
+class pointer_facade_member<Derived, T, true>
+{
+    typedef Derived<T>                      derived_type;
+    
+public:
+    template <typename U>
+    Derived<U> member(U (T::*q)) const {
+        // Calculate the offset of the member q in the struct T.
+        const std::size_t offset =
+            reinterpret_cast<mgbase::uint8_t*>(&(static_cast<T*>(MGBASE_NULLPTR)->*q))
+            - static_cast<mgbase::uint8_t*>(MGBASE_NULLPTR);
+        
+        Derived<mgbase::uint8_t> byte_ptr = pointer_core_access::cast_to<mgbase::uint8_t>(derived());
+        pointer_core_access::advance(byte_ptr, offset);
+        return pointer_core_access::cast_to<U>(byte_ptr);
+    }
+    
+private:
+          derived_type& derived()       { return static_cast<      derived_type&>(*this); }
+    const derived_type& derived() const { return static_cast<const derived_type&>(*this); }
+};
+
+template <template <typename> class Derived, typename T>
+class pointer_facade_operators
+{
+    typedef Derived<T>                      derived_type;
+    
+public:
+    derived_type operator += (std::ptrdiff_t diff) MGBASE_NOEXCEPT {
+        pointer_core_access::advance(derived(), diff);
+        return derived();
+    }
+    
+    derived_type operator + (std::ptrdiff_t diff) const MGBASE_NOEXCEPT {
+        derived_type result = derived();
+        result += diff;
+        return result;
+    }
+
+private:
+          derived_type& derived()       MGBASE_NOEXCEPT { return static_cast<      derived_type&>(*this); }
+    const derived_type& derived() const MGBASE_NOEXCEPT { return static_cast<const derived_type&>(*this); }
+};
+
+#if 0
+template <template <typename> class Derived, typename T, std::size_t Size>
+class pointer_facade_operators<Derived, T [Size]>
+{
+    typedef Derived<T[]>                    derived_type;
+    typedef Derived<T>                      normal_pointer_type;
+    
+public:
+    normal_pointer_type operator + (std::ptrdiff_t diff) const MGBASE_NOEXCEPT {
+        normal_pointer_type result = derived();
+        result += diff;
+        return result;
+    }
+
+private:
+          derived_type& derived()       MGBASE_NOEXCEPT { return static_cast<      derived_type&>(*this); }
+    const derived_type& derived() const MGBASE_NOEXCEPT { return static_cast<const derived_type&>(*this); }
+};
+#endif
+
+}
+
+template <template <typename> class Derived, typename T>
+class pointer_facade
+    : public detail::pointer_facade_member<Derived, T>
+    , public detail::pointer_facade_operators<Derived, T>
+    { };
+
+
+/*template <typename T>
+class pointer
+    : public pointer_facade<pointer, T>
+{
+private:
+    friend class pointer_core_access;
+    
+    template <typename U>
+    pointer<U> cast_to() {
+        pointer<U> result = { ptr_ };
+        return result;
+    }
+    
+    void advance(std::ptrdiff_t diff) {
+        ptr_ += diff;
+    }
+    
+    T* ptr_;
+};*/
+
+}
+
---
+++ include/mgbase/runtime_sized.hpp
@@ -0,0 +1,86 @@
+
+#pragma once
+
+#include <mgbase/lang.hpp>
+#include "pointer_facade.hpp"
+
+namespace mgbase {
+
+// runtime_sized_traits
+
+typedef std::size_t runtime_size_t;
+
+template <typename T>
+struct runtime_sized_traits {
+    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+        return sizeof(T);
+    }
+};
+
+template <typename T>
+inline MGBASE_CONSTEXPR_FUNCTION runtime_size_t runtime_size_of() MGBASE_NOEXCEPT {
+    return runtime_sized_traits<T>::size();
+}
+
+// Runtime-sized classes
+// These classes are incomplete types.
+
+template <runtime_size_t (*Size)()>
+struct runtime_sized_struct;
+
+template <typename T, runtime_size_t (*Size)()>
+struct runtime_sized_array;
+
+template <typename T1, typename T2>
+struct runtime_sized_pair;
+
+
+template <runtime_size_t (*Size)()>
+struct runtime_sized_traits< runtime_sized_struct<Size> > {
+    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+        return Size();
+    }
+};
+
+template <typename T, runtime_size_t (*Size)()>
+struct runtime_sized_traits< runtime_sized_array<T, Size> > {
+    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+        return runtime_size_of<T>() * Size();
+    }
+};
+
+template <typename T1, typename T2>
+struct runtime_sized_traits< runtime_sized_pair<T1, T2> > {
+    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+        return runtime_size_of<T1>() + runtime_size_of<T2>();
+    }
+};
+
+// get functions for runtime_sized_pair
+
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<T1> get_first(const Derived< runtime_sized_pair<T1, T2> >& ptr) {
+    return mgbase::reinterpret_pointer_cast<T1>(ptr);
+}
+
+
+template <typename T>
+inline void runtime_sized_copy_to(const T* src, T* dest) {
+    std::memcpy(dest, src, runtime_size_of<T>());
+}
+
+template <typename T>
+inline T* runtime_sized_allocate()
+{
+    mgbase::uint8_t* ptr = new mgbase::uint8_t[runtime_size_of<T>()];
+    return reinterpret_cast<T*>(ptr);
+}
+
+template <typename T>
+inline void runtime_sized_deallocate(T* ptr)
+{
+    delete[] reinterpret_cast<mgbase::uint8_t*>(ptr);
+}
+
+}
+
