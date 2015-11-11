090332be84437e7a6352e71af0eeffe57ce0ca3b
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-11-11 16:17:23 +0900

    Add runtime_sized_pointer

--- include/mgbase/runtime_sized.hpp
+++ include/mgbase/runtime_sized.hpp
@@ -3,6 +3,7 @@
 
 #include <mgbase/lang.hpp>
 #include "pointer_facade.hpp"
+#include <algorithm>
 
 namespace mgbase {
 
@@ -34,6 +35,8 @@ struct runtime_sized_array;
 template <typename T1, typename T2>
 struct runtime_sized_pair;
 
+template <typename T1, typename T2>
+struct runtime_sized_union_pair;
 
 template <runtime_size_t (*Size)()>
 struct runtime_sized_traits< runtime_sized_struct<Size> > {
@@ -44,15 +47,22 @@ struct runtime_sized_traits< runtime_sized_struct<Size> > {
 
 template <typename T, runtime_size_t (*Size)()>
 struct runtime_sized_traits< runtime_sized_array<T, Size> > {
-    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+    static runtime_size_t size() MGBASE_NOEXCEPT {
         return runtime_size_of<T>() * Size();
     }
 };
 
 template <typename T1, typename T2>
 struct runtime_sized_traits< runtime_sized_pair<T1, T2> > {
-    static MGBASE_CONSTEXPR_FUNCTION runtime_size_t size() MGBASE_NOEXCEPT {
+    static runtime_size_t size() MGBASE_NOEXCEPT {
         return runtime_size_of<T1>() + runtime_size_of<T2>();
+    }
+};
+
+template <typename T1, typename T2>
+struct runtime_sized_traits< runtime_sized_union_pair<T1, T2> > {
+    static runtime_size_t size() MGBASE_NOEXCEPT {
+        return std::max(runtime_size_of<T1>(), runtime_size_of<T2>());
     }
 };
 
@@ -62,17 +72,47 @@ template <template <typename> class Derived, typename T1, typename T2>
 inline Derived<T1> get_first(const Derived< runtime_sized_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
     return mgbase::reinterpret_pointer_cast<T1>(ptr);
 }
-
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<const T1> get_first(const Derived< const runtime_sized_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<const T1>(ptr);
+}
 template <template <typename> class Derived, typename T1, typename T2>
 inline Derived<T2> get_second(const Derived< runtime_sized_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
     return mgbase::reinterpret_pointer_cast<T2>(get_first(ptr) + 1);
 }
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<const T2> get_second(const Derived< const runtime_sized_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<const T2>(get_first(ptr) + 1);
+}
+
+// get functions for runtime_sized_union_pair
+
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<T1> get_first(const Derived< runtime_sized_union_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<T1>(ptr);
+}
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<const T1> get_first(const Derived< const runtime_sized_union_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<const T1>(ptr);
+}
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<T2> get_second(const Derived< runtime_sized_union_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<T2>(ptr);
+}
+template <template <typename> class Derived, typename T1, typename T2>
+inline Derived<const T2> get_second(const Derived< const runtime_sized_union_pair<T1, T2> >& ptr) MGBASE_NOEXCEPT {
+    return mgbase::reinterpret_pointer_cast<const T2>(ptr);
+}
+
+// get_element_at
 
 template <template <typename> class Derived, typename T, runtime_size_t (*Size)()>
 inline Derived<T> get_element_at(const Derived< runtime_sized_array<T, Size> >& ptr, std::size_t index) MGBASE_NOEXCEPT {
     MGBASE_ASSERT(index < Size());
     return mgbase::reinterpret_pointer_cast<T>(ptr) + index;
 }
+
+// runtime_sized_*
 
 template <typename T>
 inline void runtime_sized_copy_to(const T* src, T* dest) {
@@ -91,6 +131,43 @@ inline void runtime_sized_deallocate(T* ptr)
 {
     delete[] reinterpret_cast<mgbase::uint8_t*>(ptr);
 }
+
+// runtime_sized_pointer
+// alternative to ordinary pointers for runtime-sized objects
+
+template <typename T>
+class runtime_sized_pointer
+    : public mgbase::pointer_facade<mgbase::runtime_sized_pointer, T>
+{
+    typedef mgbase::pointer_facade<mgbase::runtime_sized_pointer, T>   base;
+
+public:
+    operator T* () const MGBASE_NOEXCEPT {
+        return ptr_;
+    }
+    
+    static runtime_sized_pointer create(T* ptr) {
+        runtime_sized_pointer result;
+        result.ptr_ = ptr;
+        return result;
+    }
+
+private:
+    friend class mgbase::pointer_core_access;
+    
+    template <typename U>
+    runtime_sized_pointer<U> cast_to() const MGBASE_NOEXCEPT {
+        return runtime_sized_pointer<U>::create(
+            reinterpret_cast<U*>(ptr_)
+        );
+    }
+    
+    void advance(std::ptrdiff_t index) MGBASE_NOEXCEPT {
+        ptr_ += index;
+    }
+    
+    T* ptr_;
+};
 
 }
 
