12f040e94c746e5e7ad1c3bc48df12d15621e380
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2015-12-11 22:03:08 +0900

    Update logger

--- include/mgbase/logging/logger.hpp
+++ include/mgbase/logging/logger.hpp
@@ -9,8 +9,13 @@
 
 namespace mgbase {
 
+#define MGBASE_LOG_LEVEL_DEBUG 0
+#define MGBASE_LOG_LEVEL_FATAL 1000
+
 class logger {
 public:
+    typedef mgbase::uint64_t log_level_t;
+    
     typedef std::string (*state_callback_type)();
     
     /*template <typename... Args>
@@ -31,8 +36,21 @@ public:
         else
             return std::string();
     }
+    
+    static log_level_t get_log_level() {
+        static const log_level_t result = get_log_level_from_env();
+        return result;
+    }
 
 private:
+    static log_level_t get_log_level_from_env() {
+        const char* const value = std::getenv("MGBASE_LOG_LEVEL");
+        if (value == MGBASE_NULLPTR)
+            return MGBASE_LOG_LEVEL_FATAL;
+        
+        return static_cast<log_level_t>(std::atoi(value));
+    }
+    
     // TODO: Singleton
     static state_callback_type& get_state_callback() {
         static state_callback_type callback = MGBASE_NULLPTR;
@@ -40,20 +58,18 @@ private:
     }
 };
 
-#define MGBASE_LOG_LEVEL_DEBUG 0
-#define MGBASE_LOG_LEVEL_FATAL 1000
 
-
-#ifndef MGBASE_LOG_LEVEL
+#ifndef MGBASE_ENABLE_LOG
     #if MGBASE_DEBUG
-        #define MGBASE_LOG_LEVEL    MGBASE_LOG_LEVEL_DEBUG
+        #define MGBASE_ENABLE_LOG
     #else
-        #define MGBASE_LOG_LEVEL    MGBASE_LOG_LEVEL_FATAL
+        //#define MGBASE_ENABLE_LOG // Disabled
     #endif
 #endif
 
 namespace detail {
 
+// This function is never defined.
 void logger_not_defined(...);
 
 }
@@ -62,10 +78,16 @@ void logger_not_defined(...);
 #pragma GCC diagnostic ignored "-Wvariadic-macros"
 #pragma GCC diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 
-#if MGBASE_LOG_LEVEL <= MGBASE_LOG_LEVEL_DEBUG
-    #define MGBASE_LOG_DEBUG(format, ...)  fmt::print("{}" format, ::mgbase::logger::get_state(), ## __VA_ARGS__), std::cout << std::endl
+#ifdef MGBASE_ENABLE_LOG
+    #define MGBASE_LOG_DEBUG(format, ...)  \
+        if (::mgbase::logger::get_log_level() <= MGBASE_LOG_LEVEL_DEBUG) { \
+            fmt::print("{}" format, ::mgbase::logger::get_state(), ## __VA_ARGS__); \
+            std::cout << std::endl; \
+        }
 #else
-    #define MGBASE_LOG_DEBUG(format, ...)  if (false) { mgbase::detail::logger_not_defined(format, ## __VA_ARGS__); }
+    // Do a static type check (but do nothing in run-time)
+    #define MGBASE_LOG_DEBUG(format, ...)   \
+        if (false) { mgbase::detail::logger_not_defined(format, ## __VA_ARGS__); }
 #endif
 
 #pragma GCC diagnostic pop
