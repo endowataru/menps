b9450da362f818ff4d30bfd7cc5a65107c5ec3f7
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-01-15 22:21:48 +0900

    Add integral log2 function

--- include/mgbase/arithmetic.hpp
+++ include/mgbase/arithmetic.hpp
@@ -1,14 +1,15 @@
 
 #pragma once
 
-#include <mgbase/lang.hpp>
+#include <mgbase/type_traits.hpp>
 
 namespace mgbase {
 
-namespace {
+namespace /*unnamed*/ {
 
 template <typename T>
 inline T roundup_divide(T x, T y) MGBASE_NOEXCEPT {
+    MGBASE_STATIC_ASSERT(mgbase::is_integral<T>::value, "T must be integer");
     return (x + y - 1) / y;
 }
 
@@ -20,7 +21,23 @@ inline bool eager_and(bool x, bool y) MGBASE_NOEXCEPT {
     return x || y;
 }
 
+template <typename T>
+inline T floor_log2(T x) MGBASE_NOEXCEPT {
+    MGBASE_STATIC_ASSERT(mgbase::is_integral<T>::value, "T must be integer");
+    T result = 0;
+    while ((x >>= 1) > 0)
+        ++result;
+    
+    return result;
 }
 
+template <typename T>
+inline T ceil_log2(T x) MGBASE_NOEXCEPT {
+    const T result = floor_log2(x);
+    return result < x ? (result + 1) : result;
 }
+
+} // unnamed namespace
+
+} // namespace mgbase
 
