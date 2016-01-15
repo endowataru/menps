c2d3335c23a4b4df92e74962e8404dd6285d3c7b
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-01-15 19:17:28 +0900

    Remove old codes

--- include/mgcom/collective.hpp
+++ include/mgcom/collective.hpp
@@ -89,44 +89,6 @@ inline void allgather(const T* src, T* dest, index_t number_of_elements)
 
 } // unnamed namespace
 
-
-
-
-#if 0
-
-namespace untyped {
-
-void broadcast(process_id_t root, void* ptr, index_t number_of_bytes);
-
-void allgather(const void* src, void* dest, index_t number_of_bytes);
-
-} // namespace untyped
-
-template <typename T>
-inline void broadcast(process_id_t root, T* ptr, index_t number_of_elements)
-{
-    untyped::broadcast(
-        root
-    ,   ptr
-    ,   sizeof(T) * number_of_elements
-    );
-}
-
-template <typename T>
-inline void allgather(const T* src, T* dest, index_t number_of_elements)
-{
-    // TODO: Polling of Active Messages
-    untyped::allgather(
-        src
-    ,   dest
-    ,   sizeof(T) * number_of_elements
-    );
-}
-
-void barrier();
-
-#endif
-
 } // namespace collective
 } // namespace mgcom
 
