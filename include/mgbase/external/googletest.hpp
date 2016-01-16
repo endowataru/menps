f80c34f5c92dea354411e7e761e1914e6e3127eb
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-01-17 00:09:11 +0900

    Fix for GCC

--- include/mgbase/external/googletest.hpp
+++ include/mgbase/external/googletest.hpp
@@ -8,9 +8,20 @@
     #pragma clang diagnostic ignored "-Wc++11-extensions"
 #endif
 
+#ifdef MGBASE_COMPILER_GCC
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wpedantic"
+    #pragma GCC diagnostic ignored "-Wctor-dtor-privacy"
+#endif
+
 #include <gtest/gtest.h>
 
 #ifdef MGBASE_COMPILER_CLANG
     #pragma clang diagnostic pop
 #endif
+
+#ifdef MGBASE_COMPILER_GCC
+    #pragma GCC diagnostic pop
+#endif
+
 
