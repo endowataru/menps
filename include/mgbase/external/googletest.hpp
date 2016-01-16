6390f2d4f57bd35e996ce202c3dc0ce34185d023
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-01-16 17:47:54 +0900

    Suppress warnings occurred in Google Test

---
+++ include/mgbase/external/googletest.hpp
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <mgbase/lang.hpp>
+
+#ifdef MGBASE_COMPILER_CLANG
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wc++11-extensions"
+#endif
+
+#include <gtest/gtest.h>
+
+#ifdef MGBASE_COMPILER_CLANG
+    #pragma clang diagnostic pop
+#endif
+
--- unittest/unittest.hpp
+++ unittest/unittest.hpp
@@ -1,5 +1,5 @@
 
 #pragma once
 
-#include <gtest/gtest.h>
+#include <mgbase/external/googletest.hpp>
 
