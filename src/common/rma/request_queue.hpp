c71e0b870627fc146bd1b0608d30d421a7ebcb0b
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-01-27 01:58:21 +0900

    Remove old codes

--- src/common/rma/request_queue.hpp
+++ src/common/rma/request_queue.hpp
@@ -5,7 +5,6 @@
 #include <mgbase/lockfree/mpsc_circular_buffer.hpp>
 #include <mgbase/threading/thread.hpp>
 #include <mgbase/logging/logger.hpp>
-#include <queue> // TODO
 
 namespace mgcom {
 namespace rma {
@@ -55,110 +54,11 @@ private:
                         self_->buf_.pop();
                     }
                     else {
-                        // Postpone the request.
-                        //self_->reordered_.push(*req);
-                        
                         MGBASE_LOG_DEBUG("msg:Request failed. Postponed.");
                     }
-                    //self_->buf_.pop();
                 }
-                /*else {
-                    if (!self_->reordered_.empty())
-                    {
-                        MGBASE_LOG_DEBUG("msg:Found reordered request.");
-                        
-                        Request re_req = self_->reordered_.front();
-                        self_->reordered_.pop();
-                        
-                        if (re_req(arg_)) {
-                            MGBASE_LOG_DEBUG("msg:Reordered request succeeded.");
-                        }
-                        else {
-                            self_->reordered_.push(re_req);
-                            MGBASE_LOG_DEBUG("msg:Reordered request failed. Pushed to reordered queue again.");
-                        }
-                    }
-                }*/
                 // TODO: improve external interface
                 arg_->poll();
-                //}
-                
-                /*// Check the queue.
-                if (self_->try_dequeue(&req))
-                {
-                    // Call operator().
-                    if (req(arg_)) {
-                        MGBASE_LOG_DEBUG("msg:Request succeeded.");
-                    }
-                    else {
-                        // Postpone the request.
-                        self_->reordered_.push(req);
-                        
-                        MGBASE_LOG_DEBUG("msg:Request failed. Pushed to reordered queue.");
-                    }
-                }
-                else {
-                    if (!self_->reordered_.empty())
-                    {
-                        MGBASE_LOG_DEBUG("msg:Found reordered request.");
-                        
-                        req = self_->reordered_.front();
-                        self_->reordered_.pop();
-                        
-                        if (req(arg_)) {
-                            MGBASE_LOG_DEBUG("msg:Reordered request succeeded.");
-                        }
-                        else {
-                            self_->reordered_.push(req);
-                            MGBASE_LOG_DEBUG("msg:Reordered request failed. Pushed to reordered queue again.");
-                        }
-                    }
-                    
-                    MGBASE_LOG_VERBOSE("msg:Poll.");
-                    
-                    // TODO: improve external interface
-                    arg_->poll();
-                }*/
-            #if 0
-                Request req;
-                
-                // Check the queue.
-                if (self_->try_dequeue(&req))
-                {
-                    // Call operator().
-                    if (req(arg_)) {
-                        MGBASE_LOG_DEBUG("msg:Request succeeded.");
-                    }
-                    else {
-                        // Postpone the request.
-                        self_->reordered_.push(req);
-                        
-                        MGBASE_LOG_DEBUG("msg:Request failed. Pushed to reordered queue.");
-                    }
-                }
-                else {
-                    if (!self_->reordered_.empty())
-                    {
-                        MGBASE_LOG_DEBUG("msg:Found reordered request.");
-                        
-                        req = self_->reordered_.front();
-                        self_->reordered_.pop();
-                        
-                        if (req(arg_)) {
-                            MGBASE_LOG_DEBUG("msg:Reordered request succeeded.");
-                        }
-                        else {
-                            self_->reordered_.push(req);
-                            MGBASE_LOG_DEBUG("msg:Reordered request failed. Pushed to reordered queue again.");
-                        }
-                    }
-                    
-                    MGBASE_LOG_VERBOSE("msg:Poll.");
-                    
-                    // TODO: improve external interface
-                    arg_->poll();
-                }
-            #endif
             }
         }
         
@@ -170,8 +70,6 @@ private:
     bool finished_;
     mgbase::thread th_;
     mgbase::mpsc_circular_buffer<Request, Size> buf_;
-    // TODO: avoid dynamic allocation
-    std::queue<Request> reordered_;
 };
 
 } // namespace rma
