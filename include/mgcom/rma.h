4e4ec4104d959a0a69e79c088a850e83474e51ae
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-04-14 00:59:09 +0900

    Remove many old codes

--- include/mgcom/rma.h
+++ include/mgcom/rma.h
@@ -1,5 +1,6 @@
 
 #pragma once
 
-#include <mgcom/rma/nb.h>
+#include <mgcom/rma/address.h>
+#include <mgcom/rma/pointer.h>
 
--- include/mgcom/rma.hpp
+++ include/mgcom/rma.hpp
@@ -1,7 +1,6 @@
 
 #pragma once
 
-#include <mgcom/rma/nb.hpp>
 #include <mgcom/rma/try_rma.hpp>
 #include <mgcom/rma/blocking.hpp>
 #include <mgcom/rma/cb.hpp>
