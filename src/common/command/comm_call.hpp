2274a64a0a5f8330ce15b07e652037274a95afbb
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-04-15 10:38:03 +0900

    Fix bug of mgcom::comm_call on Fujitsu compiler (retrieving function pointer of template function fails)

--- src/common/command/comm_call.hpp
+++ src/common/command/comm_call.hpp
@@ -2,8 +2,9 @@
 #pragma once
 
 #include <mgcom/common.hpp>
-#include <mgbase/basic_active_object.hpp>
+#include <mgbase/deferred.hpp>
 #include <mgbase/optional.hpp>
+#include <mgbase/logger.hpp>
 #include "basic_command.hpp"
 
 namespace mgcom {
@@ -11,75 +12,86 @@ namespace mgcom {
 template <typename R, typename Func>
 struct comm_call_cb
 {
-    mgbase::atomic<bool>        flag;
-    mgbase::value_wrapper<R>    result;
-    
     mgbase::continuation<R>     cont;
+    mgbase::wait_flag           flag;
+    
+    mgbase::value_wrapper<R>    result;
     Func                        func;
 };
 
 namespace detail {
 
-template <typename R, typename Func>
-MGBASE_ALWAYS_INLINE void execute_comm_call(comm_call_cb<R, Func>& cb)
-{
-    cb.result = mgbase::call_with_value_wrapper<R>(cb.func, mgbase::wrap_void());
-    
-    cb.flag.store(true, mgbase::memory_order_release);
-}
-
-template <typename R, typename Func>
-MGBASE_ALWAYS_INLINE mgbase::deferred<R> comm_call_check(comm_call_cb<R, Func>& cb)
-{
-    if (cb.flag.load(mgbase::memory_order_acquire))
-        return cb.result;
-    else
-        return mgbase::make_deferred(MGBASE_MAKE_INLINED_FUNCTION_TEMPLATE(comm_call_check<R, Func>), cb);
-}
-
+// Defined in another translation unit.
+MGBASE_WARN_UNUSED_RESULT
 bool try_comm_call(const mgbase::callback_function<void ()>&);
+
+template <typename R, typename Func>
+class comm_call_handlers
+{
+    typedef comm_call_cb<R, Func>   cb_type;
+    typedef mgbase::deferred<R>     result_type;
+    
+public:
+    MGBASE_ALWAYS_INLINE MGBASE_WARN_UNUSED_RESULT
+    static result_type start(cb_type& cb)
+    {
+        return mgbase::try_call_and_wait<config>(cb);
+    }
+    
+private:
+    struct config
+    {
+        typedef comm_call_handlers::cb_type         cb_type;
+        typedef comm_call_handlers::result_type     result_type;
+        
+        MGBASE_ALWAYS_INLINE MGBASE_WARN_UNUSED_RESULT
+        static bool try_call(cb_type& cb)
+        {
+            const mgbase::callback_function<void ()> f
+                = mgbase::make_callback_function(
+                    mgbase::bind1st_of_1(
+                        MGBASE_MAKE_INLINED_FUNCTION_TEMPLATE(execute)
+                    ,   mgbase::wrap_reference(cb)
+                    )
+                );
+            
+            return try_comm_call(f);
+        }
+        
+        MGBASE_ALWAYS_INLINE
+        static mgbase::wait_flag& get_flag(cb_type& cb) MGBASE_NOEXCEPT {
+            return cb.flag;
+        }
+        
+        MGBASE_ALWAYS_INLINE MGBASE_WARN_UNUSED_RESULT
+        static result_type on_complete(cb_type& cb) MGBASE_NOEXCEPT {
+            return cb.result;
+        }
+    };
+    
+    MGBASE_ALWAYS_INLINE
+    static void execute(cb_type& cb)
+    {
+        cb.result = mgbase::call_with_value_wrapper<R>(cb.func, mgbase::wrap_void());
+        
+        mgbase::set_notfiied(&cb.flag);
+    }
+};
 
 } // namespace detail
 
 template <typename R, typename Func>
 MGBASE_ALWAYS_INLINE MGBASE_WARN_UNUSED_RESULT
-mgbase::optional< mgbase::deferred<R> > try_comm_call_async(comm_call_cb<R, Func>& cb, const Func& func)
-{
-    cb.flag.store(false, mgbase::memory_order_relaxed);
-    cb.func = func;
-    
-    const mgbase::callback_function<void ()> callback_func
-        = mgbase::make_callback_function(
-            mgbase::bind1st_of_1(
-                MGBASE_MAKE_INLINED_FUNCTION_TEMPLATE(&detail::execute_comm_call<R, Func>)
-            ,   mgbase::wrap_reference(cb)
-            )
-        );
-    
-    const bool ret = detail::try_comm_call(callback_func);
-    
-    if (MGBASE_UNLIKELY(!ret))
-        return mgbase::nullopt;
-    
-    return mgbase::make_deferred(MGBASE_MAKE_INLINED_FUNCTION_TEMPLATE(&detail::comm_call_check<R, Func>), cb);
-}
-
-template <typename R, typename Func>
-MGBASE_ALWAYS_INLINE MGBASE_WARN_UNUSED_RESULT
 mgbase::deferred<R> comm_call_async(comm_call_cb<R, Func>& cb, const Func& func)
 {
-    while (true)
-    {
-        const mgbase::optional< mgbase::deferred<R> > ret
-            = try_comm_call_async<R>(cb, func);
-        
-        if (ret)
-            return *ret;
-    }
+    cb.func = func;
+    
+    return detail::comm_call_handlers<R, Func>::start(cb);
 }
 
 template <typename R, typename Func>
-MGBASE_ALWAYS_INLINE R comm_call(const Func& func)
+MGBASE_ALWAYS_INLINE
+R comm_call(const Func& func)
 {
     comm_call_cb<R, Func> cb;
     return comm_call_async<R>(cb, func).wait();
