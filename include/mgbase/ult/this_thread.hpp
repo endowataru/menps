dd8de221b3fe9f0057bdb063492d5fa185cb8dd8
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-07-14 11:32:32 +0900

    Add MGBASE_ULT_DISABLE_YIELD to disable mgbase::ult::this_thread::yield

--- include/mgbase/ult/this_thread.hpp
+++ include/mgbase/ult/this_thread.hpp
@@ -7,7 +7,14 @@ namespace mgbase {
 namespace ult {
 namespace this_thread {
 
-using namespace mgbase::this_thread;
+using mgbase::this_thread::get_id;
+
+#ifdef MGBASE_ULT_DISABLE_YIELD
+inline void yield() MGBASE_NOEXCEPT { }
+#else
+using mgbase::this_thread::yield;
+#endif
+
 
 } // namespace this_thread
 } // namespace ult
