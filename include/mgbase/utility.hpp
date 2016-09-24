5aa7e8a12ca63baefd62bbe3459baafe217f26fa
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-09-24 09:33:04 +0900

    Add using declaration of std::swap

--- include/mgbase/utility.hpp
+++ include/mgbase/utility.hpp
@@ -4,4 +4,5 @@
 #include <mgbase/utility/declval.hpp>
 #include <mgbase/utility/forward.hpp>
 #include <mgbase/utility/move.hpp>
+#include <mgbase/utility/swap.hpp>
 
---
+++ include/mgbase/utility/swap.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include <utility>
+
+namespace mgbase {
+
+using std::swap;
+
+} // namespace mgbase
+
