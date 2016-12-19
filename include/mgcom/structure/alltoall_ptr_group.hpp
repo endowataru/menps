d68e89d5d45b2e2356ac5e1369ed44e9fc9071b9
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-12-20 02:05:47 +0900

    Add alltoall_ptr_group

---
+++ include/mgcom/structure/alltoall_ptr_group.hpp
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include <mgcom/collective.hpp>
+#include <mgbase/unique_ptr.hpp>
+
+namespace mgcom {
+namespace structure {
+
+template <typename T>
+class alltoall_ptr_group
+{
+public:
+    explicit alltoall_ptr_group(T* const ptr)
+        : ptrs_(new T*[mgcom::number_of_processes()])
+    {
+        mgcom::collective::allgather(&ptr, &ptrs_[0], 1);
+    }
+    
+    T* at_process(const process_id_t proc) const MGBASE_NOEXCEPT
+    {
+        MGBASE_ASSERT(valid_process_id(proc));
+        
+        return ptrs_[proc];
+    }
+    
+private:
+    mgbase::unique_ptr<T* []> ptrs_;
+};
+
+} // namespace structure
+} // namespace mgcom
+
