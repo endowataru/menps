08ff279c1d1ddc88f36d014038a351aa2a0e5ed1
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2016-12-26 04:49:31 +0900

    Add using declarations in mgbase::ult

---
+++ include/mgbase/ult/thread.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include <mgbase/threading/thread.hpp>
+
+namespace mgbase {
+namespace ult {
+
+using mgbase::thread;
+
+} // namespace ult
+} // namespace mgbase
+
---
+++ include/mgbase/ult/unique_lock.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include <mgbase/threading/unique_lock.hpp>
+
+namespace mgbase {
+namespace ult {
+
+using mgbase::unique_lock;
+
+} // namespace ult
+} // namespace mgbase
+
