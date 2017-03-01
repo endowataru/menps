e976c2af3a12d8c79e83405a61aee07f09fdaf6b
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-03-02 03:08:47 +0900

    Fix compilation errors in old GCC

--- include/mgbase/string.hpp
+++ include/mgbase/string.hpp
@@ -2,11 +2,17 @@
 #pragma once
 
 #include <string>
+#include <sstream>
 
 namespace mgbase {
 
-using std::stoi;
-using std::stoul;
+unsigned long stoul(const std::string& str)
+{
+    std::istringstream ss(str);
+    unsigned long ret;
+    ss >> ret;
+    return ret;
+}
 
 } // namespace mgbase
 
