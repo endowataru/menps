ac3caf508b5253fd3628d17e45428a9ab6696305
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-03-05 23:04:33 +0900

    Add MGBASE_POLICY_BASED_CRTP

--- include/mgbase/crtp_base.hpp
+++ include/mgbase/crtp_base.hpp
@@ -21,3 +21,17 @@ public:
 
 } // namespace mgbase
 
+// macro-based
+
+#define MGBASE_POLICY_BASED_CRTP(Policy) \
+    private:\
+        typedef typename Policy::derived_type   derived_type; \
+        \
+        derived_type& derived() MGBASE_NOEXCEPT { \
+            return static_cast<derived_type&>(*this); \
+        } \
+        const derived_type& derived() const MGBASE_NOEXCEPT { \
+            return static_cast<const derived_type&>(*this); \
+        }
+
+
