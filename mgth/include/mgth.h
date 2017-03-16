aa48b4acc03aa31bce513d897754d965d391c795
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-03-16 17:50:38 +0900

    Fix code syntax of mgth.h

--- mgth/include/mgth.h
+++ mgth/include/mgth.h
@@ -7,28 +7,28 @@ MGBASE_EXTERN_C_BEGIN
 
 // DSM
 
-void* mgth_aligned_alloc(mgbase_size_t alignment, mgbase_size_t size);
+void * mgth_aligned_alloc(mgbase_size_t alignment, mgbase_size_t size);
 
-void* mgth_malloc(mgbase_size_t size);
+void * mgth_malloc(mgbase_size_t size);
 
-void mgth_free(void* ptr);
+void mgth_free(void * ptr);
 
 // ULT
 
 typedef struct mgth_ult_id
 {
-    void*   p;
+    void *      p;
 }
 mgth_ult_id;
 
 typedef struct mgth_new_ult
 {
     mgth_ult_id id;
-    void*       ptr;
+    void *      ptr;
 }
 mgth_new_ult;
 
-typedef void (*mgth_fork_func_t)(void*);
+typedef void (* mgth_fork_func_t)(void*);
 
 mgth_new_ult mgth_alloc_ult(mgbase_size_t alignment, mgbase_size_t size);
 
