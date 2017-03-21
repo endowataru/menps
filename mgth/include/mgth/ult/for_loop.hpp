a2de4716f8f7c3f243ae6aeac3357153219b872f
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-03-21 11:06:21 +0900

    Add for_loop_strided

--- mgth/include/mgth/ult/for_loop.hpp
+++ mgth/include/mgth/ult/for_loop.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <mgth/ult/thread.hpp>
+#include <mgbase/arithmetic.hpp>
 
 namespace mgth {
 namespace ult {
@@ -14,8 +15,8 @@ MGBASE_UNUSED parallel_execution_policy par;
 
 } // unnamed namespace
 
-template <typename I, typename F>
-void for_loop(parallel_execution_policy /*ignored*/, I first, I last, F func)
+template <typename I, typename S, typename F>
+void for_loop_strided(parallel_execution_policy /*ignored*/, I first, I last, S stride, F func)
 {
     // TODO : replace with a recursive version
     
@@ -27,17 +28,23 @@ void for_loop(parallel_execution_policy /*ignored*/, I first, I last, F func)
         }
     };
     
-    const auto n = last - first;
+    const auto n = mgbase::roundup_divide((last - first), stride);
     
     // TODO: depends on VLA, which is a compiler extension
     thread t[n];
     
     for (I i = 0; i < n; ++i) {
-        t[i] = thread(closure{ func, i+first });
+        t[i] = thread(closure{ func, (i*stride + first) });
     }
     for (I i = 0; i < n; ++i) {
         t[i].join();
     }
+}
+
+template <typename ExecutionPolicy, typename I, typename F>
+void for_loop(ExecutionPolicy&& exec, I first, I last, F func)
+{
+    for_loop_strided(mgbase::forward<ExecutionPolicy>(exec), first, last, 1, func);
 }
 
 } // namespace ult
