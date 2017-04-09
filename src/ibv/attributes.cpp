0fb9279992f53712d93cbad8a9bc2f3c31620dbe
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-04-09 18:42:27 +0900

    Fix query_device & query_port to fall back to non-exp operations

--- src/ibv/attributes.cpp
+++ src/ibv/attributes.cpp
@@ -5,19 +5,21 @@
 namespace mgdev {
 namespace ibv {
 
-#ifdef MGDEV_IBV_EXP_SUPPORTED
-    #define QUERY_DEVICE    ibv_exp_query_device
-    #define QUERY_PORT      ibv_exp_query_port
-#else
-    #define QUERY_DEVICE    ibv_query_device
-    #define QUERY_PORT      ibv_query_port
-#endif
-
 device_attr_t query_device(ibv_context* const dev)
 {
     device_attr_t attr = device_attr_t();
     
-    const auto ret = QUERY_DEVICE(dev, &attr);
+    #ifdef MGDEV_IBV_EXP_SUPPORTED
+    const auto ret_exp = ibv_exp_query_device(dev, &attr);
+    if (ret_exp == 0)
+        return attr;
+    
+    // fall back to non-exp
+    #endif
+    
+    const auto ret = ibv_query_device(dev,
+        reinterpret_cast<ibv_device_attr*>(&attr));
+    
     if (ret != 0)
         throw ibv_error("ibv_query_device() failed", ret);
     
@@ -28,7 +30,17 @@ port_attr_t query_port(ibv_context* const dev, const port_num_t port_num)
 {
     port_attr_t attr = port_attr_t();
     
-    const auto ret = QUERY_PORT(dev, port_num, &attr);
+    #ifdef MGDEV_IBV_EXP_SUPPORTED
+    const auto ret_exp = ibv_exp_query_port(dev, port_num, &attr);
+    if (ret_exp == 0)
+        return attr;
+    
+    // fall back to non-exp
+    #endif
+    
+    const auto ret = ibv_query_port(dev, port_num, 
+        reinterpret_cast<ibv_port_attr*>(&attr));
+    
     if (ret != 0)
         throw ibv_error("ibv_query_port() failed", ret);
     
@@ -40,9 +52,6 @@ bool is_only_masked_atomics(const device_attr_t& dev_attr) MGBASE_NOEXCEPT
     return (dev_attr.exp_atomic_cap & IBV_EXP_ATOMIC_HCA_REPLY_BE) &&
         (dev_attr.exp_device_cap_flags & IBV_EXP_DEVICE_EXT_ATOMICS);
 }
-
-#undef QUERY_DEVICE
-#undef QUERY_PORT
 
 } // namespace ibv
 } // namespace mgdev
