5ae6045b07c23fa9ddd6cce6ba32355d993cbaf0
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-04-11 20:23:41 +0900

    Add experimental mpsc_locked_bounded_queue

---
+++ include/mgbase/nonblocking/mpsc_locked_bounded_queue_base.hpp
@@ -0,0 +1,323 @@
+
+#pragma once
+
+#include "bounded_queue_counter.hpp"
+#include <mgbase/crtp_base.hpp>
+
+namespace mgbase {
+
+namespace detail {
+
+template <typename Element>
+struct mpsc_bounded_queue_entry
+{
+    Element                 elem;
+    mgbase::atomic<bool>    state;
+};
+
+template <typename Policy>
+struct mpsc_locked_bounded_queue_policy
+    : Policy
+{
+    typedef typename Policy::element_type           element_type;
+    
+    typedef mpsc_bounded_queue_entry<element_type>  entry_type;
+};
+
+template <typename Policy>
+class mpsc_locked_bounded_queue_base
+    : public bounded_queue_counter<mpsc_locked_bounded_queue_policy<Policy>>
+{
+    MGBASE_POLICY_BASED_CRTP(Policy)
+    
+    typedef mpsc_locked_bounded_queue_policy<Policy>    policy_type;
+    typedef bounded_queue_counter<policy_type>          base;
+    
+protected:
+    typedef typename base::index_type       index_type;
+    typedef typename base::element_type     element_type;
+    typedef typename base::entry_type       entry_type;
+    
+    typedef typename base::enqueue_transaction_base enqueue_transaction_base;
+    typedef typename base::enqueue_transaction_data enqueue_transaction_data;
+    typedef typename base::dequeue_transaction_base dequeue_transaction_base;
+    typedef typename base::dequeue_transaction_data dequeue_transaction_data;
+    
+protected:
+    mpsc_locked_bounded_queue_base() MGBASE_DEFAULT_NOEXCEPT = default;
+    
+public:
+    class enqueue_transaction
+        : public enqueue_transaction_base
+    {
+    public:
+        enqueue_transaction() MGBASE_DEFAULT_NOEXCEPT = default;
+        
+        /*implicit*/ enqueue_transaction(
+            derived_type&                   self
+        ,   const enqueue_transaction_data& data
+        ,   const bool                      sleeping
+        ) MGBASE_NOEXCEPT
+            : enqueue_transaction_base(self, data)
+            , sleeping_(sleeping)
+        { }
+        
+        enqueue_transaction(const enqueue_transaction&) = delete;
+        enqueue_transaction& operator = (const enqueue_transaction&) = delete;
+        
+        MGBASE_DEFINE_DEFAULT_MOVE_NOEXCEPT_BASE_0(enqueue_transaction, enqueue_transaction_base)
+        
+        void commit(MGBASE_UNUSED const index_type num)
+        {
+            MGBASE_ASSERT(this->valid());
+            
+            auto itr = this->entry_begin();
+            const auto last = this->entry_end();
+            
+            MGBASE_ASSERT(num == static_cast<index_type>(last - itr));
+            
+            for ( ; itr != last; ++itr)
+            {
+                // TODO: reduce memory barriers
+                itr->state.store(true, mgbase::memory_order_release);
+            }
+            
+            enqueue_transaction_base::commit();
+            
+            MGBASE_LOG_DEBUG(
+                "msg:Finished enqueuing entry.\t"
+                "from:0x{:x}\t"
+                "to:0x{:x}\t"
+            ,   this->data_.from
+            ,   this->data_.to
+            );
+        }
+        
+        bool is_sleeping() const MGBASE_NOEXCEPT {
+            return sleeping_;
+        }
+        
+    private:
+        bool sleeping_;
+    };
+    
+    enqueue_transaction try_enqueue(const index_type num)
+    {
+        auto& self = this->derived();
+        const auto cap = self.capacity();
+        
+        auto tail = this->tail_.load(mgbase::memory_order_relaxed);
+        
+        while (true)
+        {
+            const auto head = this->head_.load(mgbase::memory_order_acquire);
+            
+            MGBASE_UNUSED
+            const auto old_tail = tail;
+            
+            // The LSB (Least Significant Bit) of "tail" is used for locking.
+            // We don't use head's LSB but it's also shifted to debug easily.
+            
+            // Add num to tail and also reset the tail's LSB.
+            const auto new_tail = (tail + (num << 1)) & ~static_cast<index_type>(0x1);
+            const auto new_cap = (new_tail - head) >> 1;
+            
+            if (MGBASE_UNLIKELY(new_cap >= cap)) {
+                return {};
+            }
+            
+            // Try to store the next tail.
+            
+            // It is dangerous when other producers return the same tail,
+            // but in that case this CAS fails
+            // because loading "tail_" must precede this CAS (constrained by memory_order_acquire)
+            // and other producers must have already written the incremented value.
+            
+            const bool success =
+                this->tail_.compare_exchange_weak(
+                    tail
+                ,   new_tail
+                ,   mgbase::memory_order_acquire
+                ,   mgbase::memory_order_relaxed
+                );
+            
+            if (MGBASE_LIKELY(success))
+            {
+                MGBASE_LOG_DEBUG(
+                    "msg:Started enqueuing entry.\t"
+                    "head:0x{:x}\t"
+                    "tail:0x{:x}\t"
+                    "old_tail:0x{:x}\t"
+                    "new_tail:0x{:x}"
+                ,   head
+                ,   tail
+                ,   old_tail
+                ,   new_tail
+                );
+                
+                return enqueue_transaction(derived(),
+                    {
+                        (tail >> 1)         // from
+                    ,   (new_tail >> 1)     // to
+                    }
+                    ,   ((tail & 0x1) == 0x1) // sleeping
+                        // If the old tail's LSB was "0",
+                        // this thread found that the consumer thread is sleeping.
+                    );
+            }
+            else {
+                MGBASE_LOG_DEBUG(
+                    "msg:Failed CAS to enqueue entry.\t"
+                    "head:0x{:x}\t"
+                    "tail:0x{:x}\t"
+                    "old_tail:0x{:x}\t"
+                    "new_tail:0x{:x}"
+                ,   head
+                ,   tail
+                ,   old_tail
+                ,   new_tail
+                );
+                
+                // TODO : Avoid contention on CAS
+                //        It's better to insert an exponential back-off here
+            }
+        }
+    }
+    
+    class dequeue_transaction
+        : public dequeue_transaction_base
+    {
+    public:
+        dequeue_transaction() MGBASE_DEFAULT_NOEXCEPT = default;
+        
+        dequeue_transaction(
+            derived_type&                   self
+        ,   const dequeue_transaction_data& data
+        ) MGBASE_NOEXCEPT
+            : dequeue_transaction_base(self, data)
+        { }
+        
+        dequeue_transaction(const dequeue_transaction&) = delete;
+        dequeue_transaction& operator = (const dequeue_transaction&) = delete;
+        
+        MGBASE_DEFINE_DEFAULT_MOVE_NOEXCEPT_BASE_0(dequeue_transaction, dequeue_transaction_base)
+        
+        void commit(const index_type num)
+        {
+            // Important: num might be 0
+            
+            MGBASE_ASSERT(this->valid());
+            
+            MGBASE_ASSERT(num <= (this->data_.to - this->data_.from));
+            
+            auto itr = this->entry_begin();
+            MGBASE_UNUSED const auto last = this->entry_end();
+            
+            for (index_type i = 0; i < num; ++i, ++itr)
+            {
+                MGBASE_ASSERT(itr != last);
+                
+                // The memory barrier is done when head_ is replaced.
+                // Hence we use relaxed memory ordering here.
+                itr->state.store(false, mgbase::memory_order_relaxed);
+            }
+            
+            auto& self = *this->self_;
+            
+            const auto new_head = (this->data_.from + num) << 1;
+            
+            // Allow producers to use the element at "head".
+            // If a producer sees this modification,
+            // then processing the element on the current thread must have completed.
+            self.head_.store(new_head, mgbase::memory_order_release);
+            
+            MGBASE_LOG_DEBUG(
+                "msg:Finished dequeuing MPSC queue.\t"
+                "from:0x{:x}\t"
+                "to:0x{:x}\t"
+                "new_head:0x{:x}"
+            ,   this->data_.from
+            ,   this->data_.to
+            ,   new_head
+            );
+            
+            dequeue_transaction_base::commit();
+        }
+    };
+    
+    dequeue_transaction try_dequeue(const index_type num)
+    {
+        const auto head = this->head_.load(mgbase::memory_order_relaxed);
+        const auto tail = this->tail_.load(mgbase::memory_order_relaxed);
+        
+        auto itr = this->make_entry_iterator_at(head >> 1);
+        const auto last = this->make_entry_iterator_at(tail >> 1);
+        
+        index_type num_dequeued = 0;
+        
+        for ( ; itr != last; ++itr)
+        {
+            const bool is_visible = itr->state.load(mgbase::memory_order_acquire);
+            
+            if (MGBASE_UNLIKELY(!is_visible))
+                break;
+            
+            if (++num_dequeued >= num)
+                break;
+        }
+        
+        if (num_dequeued == 0)
+            return {};
+        else {
+            MGBASE_LOG_DEBUG(
+                "msg:Started dequeuing MPSC queue.\t"
+                "head:0x{:x}\t"
+                "tail:0x{:x}\t"
+                "num:0x{:x}"
+            ,   head
+            ,   tail
+            ,   num
+            );
+            
+            return dequeue_transaction(derived(),
+                { (head >> 1), ((head >> 1) + num_dequeued) });
+        }
+    }
+    
+    //void try_start_sleeping()
+    bool try_sleep()
+    {
+        const auto head = this->head_.load(mgbase::memory_order_relaxed);
+        auto tail = this->tail_.load(mgbase::memory_order_relaxed);
+        
+        MGBASE_ASSERT((tail & 0x1) == 0x0);
+        
+        if ((head >> 1) != (tail >> 1)) {
+            // The queue is not empty.
+            return false;
+        }
+        
+        return this->tail_.compare_exchange_weak(
+            tail
+        ,   (tail | 1)
+        ,   mgbase::memory_order_relaxed
+        ,   mgbase::memory_order_relaxed
+        );
+        
+        /*MGBASE_ASSERT((this->tail_.load(mgbase::memory_order_relaxed) & 0x1) == 0x0);
+        
+        this->tail_.fetch_add(1, mgbase::memory_order_relaxed);*/
+    }
+    
+    /*void stop_sleeping()
+    {
+        MGBASE_ASSERT((this->tail_.load(mgbase::memory_order_relaxed) & 0x1) == 0x1);
+        
+        this->tail_.fetch_sub(1, mgbase::memory_order_relaxed);
+    }*/
+};
+
+} // namespace detail
+
+} // namespace mgbase
+
---
+++ unittest/mpsc_locked_bounded_queue.cpp
@@ -0,0 +1,168 @@
+
+#include "unittest.hpp"
+#include <mgbase/nonblocking/mpsc_locked_bounded_queue.hpp>
+#include <mgbase/threading/thread.hpp>
+#include <mgbase/threading/this_thread.hpp>
+#include <mgbase/mutex.hpp>
+#include <mgbase/condition_variable.hpp>
+#include <mgbase/unique_ptr.hpp>
+
+TEST(MpscLockedBoundedQueue, Serial)
+{
+    const mgbase::uint64_t N = 10000;
+    
+    mgbase::static_mpsc_locked_bounded_queue<mgbase::uint64_t, 256> buf;
+    
+    mgbase::uint64_t x = 0;
+    for (mgbase::uint64_t i = 1; i <= N; ++i) {
+        {
+            auto t = buf.try_enqueue(1);
+            ASSERT_TRUE(t.valid());
+            *t.begin() = i;
+            t.commit(1);
+            
+            ASSERT_FALSE(t.is_sleeping());
+        }
+        
+        x += buf.dequeue();
+    }
+    
+    ASSERT_EQ(N * (N+1) / 2, x);
+}
+
+TEST(MpscLockedBoundedQueue, Serial2)
+{
+    const mgbase::uint64_t N = 10000;
+    
+    mgbase::static_mpsc_locked_bounded_queue<mgbase::uint64_t, 256> buf;
+    
+    //buf.start_sleeping();
+    ASSERT_TRUE(buf.try_sleep());
+    
+    mgbase::uint64_t x = 0;
+    for (mgbase::uint64_t i = 1; i <= N; ++i) {
+        {
+            auto t = buf.try_enqueue(1);
+            ASSERT_TRUE(t.valid());
+            *t.begin() = i;
+            t.commit(1);
+            
+            ASSERT_EQ(i == 1, t.is_sleeping());
+        }
+        
+        x += buf.dequeue();
+    }
+    
+    ASSERT_EQ(N * (N+1) / 2, x);
+}
+
+namespace /*unnamed*/ {
+
+template <typename Queue>
+struct functor
+{
+    Queue& q;
+    mgbase::uint64_t n;
+    mgbase::mutex& mtx;
+    mgbase::condition_variable& cv;
+    
+    void operator() ()
+    {
+        for (mgbase::uint64_t i = 1; i <= n; ++i) {
+            do {
+                auto t = q.try_enqueue(1); // CAS: tail += 0x2;
+                if (!t.valid() || t.size() == 0) {
+                    mgbase::this_thread::yield();
+                    continue;
+                }
+                
+                *t.begin() = i;
+                t.commit(1); // entry.flag = true;
+                
+                if (t.is_sleeping()) { // (old_tail & 1) == 1
+                    mgbase::unique_lock<mgbase::mutex> lk(mtx);
+                    cv.notify_one();
+                }
+            }
+            while (false);
+        }
+    }
+};
+
+} // unnamed namespace
+
+TEST(MpscLockedBoundedQueue, SpSc)
+{
+    typedef mgbase::static_mpsc_locked_bounded_queue<mgbase::uint64_t, 256>  queue_type;
+    
+    const mgbase::uint64_t N = 100000;
+    
+    queue_type buf;
+    mgbase::mutex mtx;
+    mgbase::condition_variable cv;
+    
+    mgbase::thread th{functor<queue_type>{buf, N, mtx, cv}};
+    
+    mgbase::uint64_t x = 0;
+    {
+        mgbase::unique_lock<mgbase::mutex> lk(mtx);
+        for (mgbase::uint64_t i = 1; i <= N; ++i) {
+            do {
+                auto t = buf.try_dequeue(1); // check queue size & entry flag
+                if (!t.valid() || t.size() == 0) { // (head&~1) == (tail&~1) ?
+                    if (buf.try_sleep()) { // CAS: tail |= 1
+                        cv.wait(lk);
+                    }
+                    else  {
+                        mgbase::this_thread::yield();
+                    }
+                    
+                    continue;
+                }
+                
+                x += *t.begin();
+                
+                t.commit(1); // head += 0x2;
+            }
+            while (false);
+        }
+    }
+    
+    th.join();
+    
+    ASSERT_EQ(N * (N+1) / 2, x);
+}
+
+#if 0
+
+TEST(MpscLockedBoundedQueue, MpSc)
+{
+    const mgbase::uint64_t N = 10000;
+    const mgbase::size_t num_threads = 10;
+    
+    typedef mgbase::mpsc_locked_bounded_queue<mgbase::uint64_t, 256>  queue_type;
+    
+    queue_type buf;
+    
+    mgbase::scoped_ptr<mgbase::thread []> ths(new mgbase::thread[num_threads]);
+    
+    for (mgbase::size_t i = 0; i < num_threads; ++i)
+        ths[i] = mgbase::thread{functor<queue_type>{buf, N}};
+    
+    mgbase::uint64_t x = 0;
+    for (mgbase::uint64_t i = 0; i < num_threads * N; ++i) {
+        x += buf.dequeue();
+    }
+    
+    for (mgbase::size_t i = 0; i < num_threads; ++i)
+        ths[i].join();
+    
+    //ASSERT_TRUE(buf.peek() == MGBASE_NULLPTR);
+    //ASSERT_FALSE(buf.peek(&head));
+    
+    ASSERT_EQ(num_threads * N * (N+1) / 2, x);
+}
+
+
+#endif
+
