731037b98ec808bc014a005d3caf4c2fb4ce92d1
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-05-10 10:08:12 +0900

    Add special function ult::fork_parent_first_detached for mgcom

--- include/mgult/backend/mth/thread.hpp
+++ include/mgult/backend/mth/thread.hpp
@@ -90,6 +90,10 @@ public:
         );
     }
     
+    explicit thread(const myth_thread_t t)
+        : t_(t)
+    { }
+    
     thread(const thread&) = delete;
     thread& operator = (const thread&) = delete;
     
@@ -119,6 +123,26 @@ public:
 private:
     thread_ptr_type t_;
 };
+
+inline myth_thread_attr_t make_parent_first_detached_thread_attr()
+{
+    myth_thread_attr_t attr = myth_thread_attr_t();
+    myth_thread_attr_init(&attr);
+    myth_thread_attr_setdetachstate(&attr, 1);
+    //attr.child_first = 0;
+    return attr;
+}
+
+// special function for mgcom
+inline void fork_parent_first_detached(const myth_func_t func, void* const ptr)
+{
+    static myth_thread_attr_t attr = make_parent_first_detached_thread_attr();
+    myth_thread_t t;
+    
+    MGBASE_UNUSED
+    const int ret = myth_create_ex(&t, &attr, func, ptr);
+    // Note: ret is not checked because current MassiveThreads only returns 0
+}
 
 } // namespace mth
 } // namespace backend
