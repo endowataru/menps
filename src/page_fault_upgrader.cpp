c90a6f206dc01cd1f8add64a84ee704b904ee2f9
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2017-06-27 10:17:45 +0900

    Use ult::thread_specific in page_fault_upgrader

--- src/page_fault_upgrader.cpp
+++ src/page_fault_upgrader.cpp
@@ -2,8 +2,11 @@
 #include "page_fault_upgrader.hpp"
 #include "sigsegv_catcher.hpp"
 #include "sigbus_catcher.hpp"
+#include <mgdsm/ult.hpp>
 
 namespace mgdsm {
+
+#define MGDSM_USE_THREAD_SPECIFIC
 
 class page_fault_upgrader::impl
 {
@@ -16,11 +19,21 @@ public:
     
     void enable_on_this_thread()
     {
+        #ifdef MGDSM_USE_THREAD_SPECIFIC
+        delete tls_.get();
+        tls_.set(new tls_data{0});
+        #else
         is_upgrade_enabled_ = true;
+        #endif
     }
     void disable_on_this_thread()
     {
+        #ifdef MGDSM_USE_THREAD_SPECIFIC
+        delete tls_.get();
+        tls_.set(MGBASE_NULLPTR);
+        #else
         is_upgrade_enabled_ = false;
+        #endif
     }
     
 private:
@@ -30,6 +43,16 @@ private:
         
         bool operator () (void* const ptr) const
         {
+            #ifdef MGDSM_USE_THREAD_SPECIFIC
+            auto p = self.tls_.get();
+            if (p == MGBASE_NULLPTR) {
+                return false;
+            }
+            
+            if (++p->rec_count > 1) {
+                return false;
+            }
+            #else
             if (!is_upgrade_enabled_) {
                 return false;
             }
@@ -37,6 +60,7 @@ private:
             if (++rec_count_ > 1) {
                 return false;
             }
+            #endif
             
             if (ptr == MGBASE_NULLPTR) {
                 return false;
@@ -44,7 +68,11 @@ private:
             
             self.conf_.sp.do_for_block_at(ptr, upgrade_callback{});
             
+            #ifdef MGDSM_USE_THREAD_SPECIFIC
+            --p->rec_count;
+            #else
             --rec_count_;
+            #endif
             
             return true;
         }
@@ -83,13 +111,27 @@ private:
     
     sigsegv_catcher segv_catch_;
     sigbus_catcher bus_catch_;
+ 
+    #ifdef MGDSM_USE_THREAD_SPECIFIC
+    struct tls_data {
+        mgbase::size_t  rec_count;
+    };
+    struct tls_policy {
+        typedef tls_data    value_type;
+    };
     
+    mgdsm::ult::thread_specific<tls_policy> tls_;
+    
+    #else
     static MGBASE_THREAD_LOCAL bool is_upgrade_enabled_;
     static MGBASE_THREAD_LOCAL mgbase::size_t rec_count_;
+    #endif
 };
 
+#ifndef MGDSM_USE_THREAD_SPECIFIC
 MGBASE_THREAD_LOCAL bool page_fault_upgrader::impl::is_upgrade_enabled_ = false;
 MGBASE_THREAD_LOCAL mgbase::size_t page_fault_upgrader::impl::rec_count_ = 0;
+#endif
 
 
 page_fault_upgrader::page_fault_upgrader(const config& conf)
