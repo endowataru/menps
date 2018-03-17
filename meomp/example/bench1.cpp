9eaf4ffbce47f51b3f9198d580ee6f3ef5e94320
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-03-18 06:39:08 +0900

    Add simple microbenchmark in meomp

---
+++ meomp/example/bench1.cpp
@@ -0,0 +1,37 @@
+ 
+#include <menps/meomp.hpp>
+#include <menps/mefdn/profiling/average_accumulator.hpp>
+#include <menps/mefdn/profiling/clock.hpp>
+#include <menps/mefdn/external/fmt.hpp>
+
+extern "C"
+int meomp_main(int argc, char** argv)
+{
+    using fmt::print;
+    using menps::mefdn::average_accumulator;
+    using menps::mefdn::get_cpu_clock;
+    using menps::mefdn::cpu_clock_t;
+    
+    average_accumulator<cpu_clock_t> acc;
+    
+    #pragma omp parallel
+    {
+        const auto num_trials = std::atoi(argv[1]);
+        
+        for (int i = 0; i < num_trials; ++i) {
+            const auto t0 = get_cpu_clock();
+            
+            #pragma omp barrier
+            
+            const auto t1 = get_cpu_clock();
+            
+            acc.add(t1-t0);
+        }
+    }
+    
+    print("result: {}\n", acc.summary());
+    
+    return 0;
+}
+ 
+ 
