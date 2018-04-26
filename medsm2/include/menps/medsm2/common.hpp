015c9ef672215daa228b5bd5c020ca83adce94b0
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-04-27 00:48:48 +0900

    Add medsm2::default_ult_itf

--- medsm2/include/menps/medsm2/common.hpp
+++ medsm2/include/menps/medsm2/common.hpp
@@ -1,7 +1,14 @@
 
 #pragma once
 
-#include <menps/mefdn/lang.hpp>
+#include <menps/mecom2/common.hpp>
 
 #define MEDSM2_ENABLE_P2P_LOCK
 
+namespace menps {
+namespace medsm2 {
+
+using default_ult_itf = mecom2::default_ult_itf;
+
+} // namespace medsm2
+} // namespace menps
