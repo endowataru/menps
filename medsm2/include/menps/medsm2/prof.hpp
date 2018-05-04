76dc09b31fdc18b5ada6e2241b26d184970c1bc3
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-05-04 21:56:24 +0900

    Fix medsm2::prof to guard accumulator by spinlock

--- medsm2/include/menps/medsm2/prof.hpp
+++ medsm2/include/menps/medsm2/prof.hpp
@@ -6,11 +6,10 @@
 #include <menps/mefdn/profiling/clock.hpp>
 #include <menps/mefdn/assert.hpp>
 #include <menps/mefdn/external/fmt.hpp>
+#include <menps/mefdn/thread/spinlock.hpp>
 
 namespace menps {
 namespace medsm2 {
-
-using clock_accumulator = mefdn::average_accumulator<mefdn::cpu_clock_t>;
 
 #define MEDSM2_PROF_KINDS(x) \
     x(fence) \
@@ -30,8 +29,23 @@ enum class prof_kind {
 
 class prof
 {
+#ifdef MEDSM2_ENABLE_PROF
+private:
+    using spinlock_type = mefdn::spinlock;
+    using accumulator_type = mefdn::average_accumulator<mefdn::cpu_clock_t>;
+    
+    struct entry {
+        spinlock_type       lock;
+        accumulator_type    acc;
+    };
+    
+    static entry& get(const prof_kind k) {
+        static entry arr[static_cast<mefdn::size_t>(prof_kind::end)];
+        MEFDN_ASSERT(k < prof_kind::end);
+        return arr[static_cast<mefdn::size_t>(k)];
+    }
+    
 public:
-    #ifdef MEDSM2_ENABLE_PROF
     static mefdn::cpu_clock_t start()
     {
         return mefdn::get_cpu_clock();
@@ -40,24 +54,9 @@ public:
     static void finish(const prof_kind k, mefdn::cpu_clock_t c)
     {
         const auto now = mefdn::get_cpu_clock();
-        prof::get(k).add(now-c);
-    }
-    #else
-    static mefdn::cpu_clock_t start()
-    {
-        return 0;
-    }
-    
-    static void finish(const prof_kind /*k*/, mefdn::cpu_clock_t /*c*/)
-    {
-        // Do nothing.
-    }
-    #endif
-    
-    static clock_accumulator& get(const prof_kind k) {
-        static clock_accumulator arr[static_cast<mefdn::size_t>(prof_kind::end)];
-        MEFDN_ASSERT(k < prof_kind::end);
-        return arr[static_cast<mefdn::size_t>(k)];
+        auto& e = prof::get(k);
+        mefdn::lock_guard<spinlock_type> lk(e.lock);
+        e.acc.add(now-c);
     }
     
     static std::string to_string(const char* const head)
@@ -65,7 +64,7 @@ public:
         fmt::MemoryWriter w;
         
         #define WRITE(name) \
-            w.write("{}" #name ": {}\n", head, prof::get(prof_kind::name));
+            w.write("{}" #name ": {}\n", head, prof::get(prof_kind::name).acc);
         
         MEDSM2_PROF_KINDS(WRITE)
         
@@ -73,6 +72,20 @@ public:
         
         return w.str();
     }
+#else
+public:
+    static mefdn::cpu_clock_t start() {
+        return 0;
+    }
+    
+    static void finish(const prof_kind /*k*/, mefdn::cpu_clock_t /*c*/) {
+        // Do nothing.
+    }
+    
+    static std::string to_string(const char* const /*head*/) {
+        return "";
+    }
+#endif
 };
 
 } // namespace medsm2
