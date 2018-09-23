9734cf5543c0242878e73838ae2db8bb7102fdef
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-09-24 07:07:54 +0900

    Remove division of medsm2::svm_seg_table::get_seg_id()

--- medsm2/include/menps/medsm2/svm/svm_seg_table.hpp
+++ medsm2/include/menps/medsm2/svm/svm_seg_table.hpp
@@ -37,6 +37,7 @@ public:
         : base(conf)
         //, max_seg_size_(conf.max_seg_size)
         , max_seg_size_(P::constants_type::max_seg_size) // TODO
+        , ln2_max_seg_size_(mefdn::floor_log2(max_seg_size_))
     {
         MEFDN_ASSERT(mefdn::is_power_of_2(max_seg_size_));
     }
@@ -46,11 +47,17 @@ private:
     
     seg_id_type get_seg_id(const blk_id_type blk_id)
     {
-        // TODO: Remove division (use bit operation)
-        return blk_id / this->max_seg_size_;
+        // Shift the block ID to divide.
+        const auto ret = blk_id >> this->ln2_max_seg_size_;
+        
+        // Check that the division is correct.
+        MEFDN_ASSERT(ret == blk_id / max_seg_size_);
+        
+        return ret;
     }
     
     const size_type max_seg_size_;
+    const size_type ln2_max_seg_size_;
 };
 
 } // namespace medsm2
