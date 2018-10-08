1b599cdf0877f8b51ac0e701d20fb81bd25a9426
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-10-08 12:32:57 +0900

    Add flag -O0 to UCX in Debug build

--- medev2/cmake/ucx.cmake
+++ medev2/cmake/ucx.cmake
@@ -2,6 +2,55 @@ cmake_minimum_required(VERSION 3.0)
 
 include(ExternalProject)
 
+# Note: UCX's configure adds "-g" by default (BASE_CFLAGS)
+
+if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    set(MEDEV2_UCX_CONFIGURE_FLAGS
+        --enable-compiler-opt=none # == "-O0"
+        --enable-debug
+        
+        #--enable-gtest             # Disabled to shorten build time
+        #--with-valgrind            # TODO: for ReedBush
+        #--enable-profiling         # Disabled
+        #--enable-frame-pointer     # TODO
+        #--enable-stats             # Disabled
+        #--enable-memtrack          # Disabled
+        #--enable-fault-injection   # Disabled; seemed meaningless
+        --enable-debug-data
+        #--enable-mt                # Disabled
+    )
+    set(MEDEV2_UCX_CFLAGS "-ftls-model=initial-exec -Wall")
+elseif (${CMAKE_BUILD_TYPE} STREQUAL "RelWithDebInfo")
+    set(MEDEV2_UCX_CONFIGURE_FLAGS
+        --enable-compiler-opt
+        --disable-debug # TODO
+        
+        --enable-optimizations
+        --disable-logging
+        --disable-debug
+        --disable-assertions
+        --disable-params-check
+    )
+    set(MEDEV2_UCX_CFLAGS "-ftls-model=initial-exec -Wall")
+else()
+    set(MEDEV2_UCX_CONFIGURE_FLAGS
+        --enable-compiler-opt
+        --disable-debug
+        
+        --enable-optimizations
+        --disable-logging
+        --disable-debug
+        --disable-assertions
+        --disable-params-check
+    )
+    set(MEDEV2_UCX_CFLAGS "-ftls-model=initial-exec -Wall")
+endif()
+
+list(APPEND MEDEV2_UCX_CONFIGURE_FLAGS --without-cuda)
+# TODO: Including <cuda.h> fails on ReedBush
+
+configure_file(cmake/ucx-configure.sh.in ucx-configure.sh @ONLY)
+
 ExternalProject_Add(UCX
     URL ${PROJECT_SOURCE_DIR}/external/ucx
     
@@ -10,12 +59,13 @@ ExternalProject_Add(UCX
     #    "s/^noinst_PROGRAMS       = test_memhooks shmem_pingpong/noinst_PROGRAMS       = test_memhooks/"
     #    <SOURCE_DIR>/test/mpi/Makefile.am
     
-    CONFIGURE_COMMAND <SOURCE_DIR>/contrib/configure-release --prefix=<INSTALL_DIR>
-        --without-cuda
-        # TODO: including <cuda.h> fails on ReedBush
+    CONFIGURE_COMMAND /bin/sh
+        ${CMAKE_CURRENT_BINARY_DIR}/ucx-configure.sh
+        <SOURCE_DIR>/configure
+        --prefix=<INSTALL_DIR>
+        ${MEDEV2_UCX_CONFIGURE_FLAGS}
     
-    #--with-mpi
-    #    --disable-numa
+    BUILD_COMMAND $(MAKE) V=1
 )
 
 ExternalProject_Add_Step(UCX autogen
