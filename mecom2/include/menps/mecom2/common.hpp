f82756e38577de6f9d41830488a5f0321853aea8
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-11-05 05:39:41 +0900

    Add MECOM2_USE_QDLOCK (disabled by default)

--- mecom2/include/menps/mecom2/common.hpp
+++ mecom2/include/menps/mecom2/common.hpp
@@ -4,8 +4,6 @@
 #include <menps/medev2/common.hpp>
 #include <menps/mecom2/config.h>
 
-//#define MECOM2_USE_QDLOCK
-
 #ifdef MECOM2_USE_QDLOCK
     #include <menps/meult/qd/qdlock_mutex.hpp>
 #endif
