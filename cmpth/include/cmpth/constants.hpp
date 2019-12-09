0efdb3d8c1011fc05f70a78d3cbf28928ebd710f
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2019-12-09 17:35:30 +0900

    Increase cmpth::constants::default_mcs_pool_threshold from 4 to 64

--- cmpth/include/cmpth/constants.hpp
+++ cmpth/include/cmpth/constants.hpp
@@ -9,7 +9,7 @@ struct constants
 {
     static constexpr const fdn::size_t default_stack_size = 1 << 16;
     static constexpr const fdn::size_t default_task_pool_threshold = 4;
-    static constexpr const fdn::size_t default_mcs_pool_threshold = 4;
+    static constexpr const fdn::size_t default_mcs_pool_threshold = 64;
     static constexpr const fdn::size_t default_ring_buf_length = 100;
     static constexpr const fdn::size_t default_worker_deque_size = 4096;
     static constexpr bool call_myth_uncond_setup = false;
